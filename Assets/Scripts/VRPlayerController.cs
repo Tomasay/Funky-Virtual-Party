a0bbe3024b86cd4600a7933b375b4a7d0159d78a
Thomas Ratliff <42788482+Thomas@users.noreply.github.com>
2021-04-21 23:32:43 -0400

    added vr plugins, as well as first iteration of moving by pumping hands

---
+++ Assets/Scripts/VRPlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayerController : MonoBehaviour
+{
+    private GameObject leftHand, rightHand;
+
+    [SerializeField]
+    GameObject forwardDirection;
+
+    private Vector3 leftHandPos, rightHandPos; //Used to store previous frame hand positions
+    private float handDistance = 0;
+    private float handMovementSpeed = 10;
+
+    public GameObject LeftHand { get => leftHand; set => leftHand = value; }
+    public GameObject RightHand { get => rightHand; set => rightHand = value; }
+
+    private bool handMovement = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (handMovement)
+        {
+            handDistance = Vector3.Distance(leftHandPos, LeftHand.transform.localPosition) + Vector3.Distance(rightHandPos, RightHand.transform.localPosition);
+
+            leftHandPos = LeftHand.transform.localPosition;
+            rightHandPos = RightHand.transform.localPosition;
+
+            if (handDistance < 1 && handDistance > 0)
+                transform.position -= forwardDirection.transform.forward * handDistance * handMovementSpeed * Time.deltaTime;
+        }
+    }
+}
