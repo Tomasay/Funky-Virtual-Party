08c044fb1cb77cb893b792985a5f3e3e253fc742
Thomas Ratliff <42788482+Thomas@users.noreply.github.com>
2021-05-02 16:07:48 -0400

    Added game states and countdown timer

--- Assets/Scripts/ClientPlayer.cs
+++ Assets/Scripts/ClientPlayer.cs
@@ -12,10 +12,13 @@ public class ClientPlayer : MonoBehaviour
     private Color playerColor = Color.clear;
     private Vector3 movement;
     private float speed = 5;
+    private bool canMove = true;
 
     public string PlayerID { get => playerID; set => playerID = value; }
     public string PlayerName { get => playerName; set { playerNameText.text = playerName = value; } }
     public Color PlayerColor { get => playerColor; set{ playerColor = value; GetComponent<Renderer>().material.SetColor("_BaseColor", value); } }
+
+    public bool CanMove { get => canMove; set => canMove = value; }
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +41,10 @@ public class ClientPlayer : MonoBehaviour
 
     public void Move(int x, int y)
     {
-        movement = new Vector3((x / 100.0f) * speed, 0, (y / 100.0f) * speed);
-        Debug.Log("SPEED: " + movement);
+        if (canMove)
+        {
+            movement = new Vector3((x / 100.0f) * speed, 0, (y / 100.0f) * speed);
+            Debug.Log("SPEED: " + movement);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    GameObject playerPrefab;
+    [SerializeField] GameObject playerPrefab;
 
-    [SerializeField]
-    Transform[] playerSpawns;
+    [SerializeField] Transform[] playerSpawns;
+
+    private const int COUNTDOWN_AMOUNT = 5;
+    [SerializeField] private TMP_Text countdownText;
+
+    private enum GameState
+    {
+        Countdown,
+        GameLoop,
+        PlayerCaptured,
+        TimeEnded
+    }
+
+    private GameState state;
 
     void Start()
     {
@@ -23,12 +35,44 @@ public class GameManager : MonoBehaviour
         }
 
         ClientManager.instance.SpawnPlayers(playerPrefab, playerSpawns);
+
+        state = GameState.Countdown;
+        SetPlayerMovement(false);
+        StartCoroutine("StartCountdownTimer", COUNTDOWN_AMOUNT);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case GameState.Countdown:
+                break;
+            case GameState.GameLoop:
+                break;
+            case GameState.PlayerCaptured:
+                break;
+            case GameState.TimeEnded:
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator StartCountdownTimer(int countdown)
+    {
+        for (int i = countdown; i > 0; i--)
+        {
+            countdownText.text = "" + i;
+            yield return new WaitForSeconds(1);
+        }
+
+        countdownText.text = "GO!";
+        yield return new WaitForSeconds(1);
+        countdownText.enabled = false;
+
+        state = GameState.GameLoop;
+        SetPlayerMovement(true);
     }
 
     IEnumerator DisableMirroring()
@@ -36,5 +80,13 @@ public class GameManager : MonoBehaviour
         yield return new WaitForEndOfFrame();
         //UnityEngine.VR.VRSettings.showDeviceView = false;
         XRSettings.showDeviceView = false;
+    }
+
+    private void SetPlayerMovement(bool canPlayerMove)
+    {
+        foreach (KeyValuePair<string, GameObject> p in ClientManager.instance.Players)
+        {
+            ClientManager.instance.Players[p.Key].GetComponent<ClientPlayer>().CanMove = canPlayerMove;
+        }
     }
 }
