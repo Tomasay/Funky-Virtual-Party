9c3194364859f7f6abf05336d36814c30fa266a8
Thomas Ratliff <42788482+Thomas@users.noreply.github.com>
2021-08-19 16:55:11 -0400

    Moved display logic to MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,6 +24,13 @@ public class MainMenu : MonoBehaviour
         {
             qrCode.texture = qr.generate_qr_code(ClientManager.instance.URL + "/?partyCode=" + ClientManager.instance.Passcode);
         }
+
+        // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
+        // Check if additional displays are available and activate each.
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
     }
 
     private void SpawnPlayer(GameObject player)
