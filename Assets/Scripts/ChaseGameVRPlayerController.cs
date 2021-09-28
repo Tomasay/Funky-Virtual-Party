07bcaae98cac4765b43ece9cab8ba7aff8371813
Thomas Ratliff <42788482+Tomasay@users.noreply.github.com>
2021-09-27 23:10:16 -0400

    Added sprint meter, vr player can only sprint when meter is full enough

--- Assets/Scripts/ChaseGameVRPlayerController.cs
+++ Assets/Scripts/ChaseGameVRPlayerController.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Autohand;
+using UnityEngine.UI;
 
 public class ChaseGameVRPlayerController : VRPlayerController
 {
+    [SerializeField] private float handMovementSpeed = 20;
+    [SerializeField] private Image sprintMeter;
+
     private Vector3 movement, leftHandPos, rightHandPos; //Used to store previous frame hand positions
     private float handDistance = 0;
-    [SerializeField] private float handMovementSpeed = 20;
+    private float sprintAmount, maxSprint = 3, minimumSprintPercent = 0.5f;
+    private bool handMovement = true, sprintCooldown;
 
-    private bool handMovement = true;
+    private void Start()
+    {
+        sprintAmount = maxSprint;
+    }
 
     void Update()
     {
-        if (handMovement && ahp.handLeft.IsSqueezing() && ahp.handRight.IsSqueezing())
+        if (handMovement && !sprintCooldown && ahp.handLeft.IsSqueezing() && ahp.handRight.IsSqueezing())
         {
             handDistance = Vector3.Distance(leftHandPos, ahp.handLeft.transform.localPosition) + Vector3.Distance(rightHandPos, ahp.handRight.transform.localPosition);
 
@@ -24,7 +32,31 @@ public class ChaseGameVRPlayerController : VRPlayerController
             {
                 movement = Vector3.Lerp(movement, -(forwardDirection.transform.forward * handDistance * handMovementSpeed * Time.deltaTime), 5 * Time.deltaTime);
                 ahp.AddMove(movement);
+
+                sprintAmount = Mathf.Max(sprintAmount - Time.deltaTime, 0);
+            }
+
+            if(sprintAmount / maxSprint < 0.05f)
+            {
+                sprintCooldown = true;
+                sprintMeter.color = Color.red;
             }
         }
+        else
+        {
+            sprintAmount = Mathf.Min(sprintAmount + Time.deltaTime, maxSprint);
+
+            if(sprintCooldown && sprintAmount / maxSprint > minimumSprintPercent)
+            {
+                sprintCooldown = false;
+            }
+            else if(sprintCooldown)
+            {
+                sprintMeter.color = Color.Lerp(Color.red, Color.white, (sprintAmount / maxSprint) / minimumSprintPercent);
+            }
+        }
+
+        //Update sprint sprite
+        sprintMeter.fillAmount = sprintAmount / maxSprint;
     }
 }
