d6537d088aa74fbd98f3f4f77db81c0a75e277ff
Thomas Ratliff <42788482+Tomasay@users.noreply.github.com>
2021-11-29 23:03:31 -0500

    Chase game timer improvements, more shapes recompilation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] GameObject VRPlayer;
 
-    private const int COUNTDOWN_AMOUNT = 5, GAME_TIME_AMOUNT = 60;
+    private const int COUNTDOWN_AMOUNT = 10, GAME_TIME_AMOUNT = 60;
     [SerializeField] private TMP_Text countdownText, vrInfoText, gameTimeText, vrGameTimeText;
     private bool countingDown = false;
     private float timeRemaining;
@@ -62,9 +62,14 @@ public class GameManager : MonoBehaviour
                 timeRemaining -= Time.deltaTime;
                 gameTimeText.text = FormatTime(timeRemaining);
                 vrGameTimeText.text = FormatTime(timeRemaining);
-                if (timeRemaining <= 0)
+                if (timeRemaining <= 10) //Display final 10 seconds for VR player
+                {
+                    vrGameTimeText.GetComponent<Animator>().SetBool("Pulsate", true);
+                }
+                if (timeRemaining <= 0) //Game end, VR player wins
                 {
                     State = GameState.TimeEnded;
+                    vrGameTimeText.GetComponent<Animator>().SetBool("Pulsate", false);
                 }
                 break;
             case GameState.PlayerCaptured:
@@ -80,8 +85,12 @@ public class GameManager : MonoBehaviour
 
     IEnumerator StartCountdownTimer(int countdown)
     {
-        SetVRPlayerMovement(true);
         countingDown = true;
+
+        vrInfoText.text = "RUN!";
+        SetVRPlayerMovement(true);
+        yield return new WaitForSeconds(1);
+
         for (int i = countdown; i > 0; i--)
         {
             countdownText.text = "" + i;
@@ -90,19 +99,22 @@ public class GameManager : MonoBehaviour
         }
 
         countdownText.text = "GO!";
-        vrInfoText.text = "GO!";
+        
         yield return new WaitForSeconds(1);
         countdownText.enabled = false;
         vrInfoText.text = "";
 
         State = GameState.GameLoop;
         SetPlayerMovement(true);
+
+        countingDown = false;
     }
 
     IEnumerator GameOver(int countdown, string txt)
     {
         state = GameState.GameOver;
 
+        vrInfoText.text = "YOU WIN!";
         countdownText.enabled = true;
         countdownText.text = txt;
         yield return new WaitForSeconds(3);
