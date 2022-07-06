0262773881c2ef2b9fbf177e28f50b164b3e1fb7
Thomas Ratliff <42788482+Tomasay@users.noreply.github.com>
2022-07-05 20:16:39 -0400

    Vinyl disc system!

---
+++ Assets/Scripts/Main Menu/VinylInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VinylInfo : MonoBehaviour
+{
+    [SerializeField] public string SceneToLoad;
+    [SerializeField] public string Title, Description;
+    [SerializeField] public VideoClip videoPreview;
+}
---
+++ Assets/Scripts/Main Menu/VinylPlayer.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Autohand;
+using TMPro;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VinylPlayer : MonoBehaviour
+{
+    [SerializeField] ClientManager cm;
+
+    [SerializeField] Transform vinylParent;
+    [SerializeField] float vinylSpinSpeed = 0.2f;
+
+    [SerializeField] TMP_Text titleText, descriptionText;
+    [SerializeField] VideoPlayer videoPlayer;
+
+    private Animator anim;
+    private GameObject currentVinyl = null;
+
+    private void Start()
+    {
+        if(!TryGetComponent<Animator>(out anim))
+        {
+            Debug.LogWarning("No animator found on vinyl player!");
+        }
+
+        titleText.enabled = false;
+        descriptionText.enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!currentVinyl && other.gameObject.name.Contains("Vinyl")) //If vinyl is in trigger, and there is no vinyl on the player
+        {
+            Grabbable g = other.gameObject.GetComponent<Grabbable>();
+
+            if (!g.IsHeld()) //If vinyl is released from hands
+            {
+                currentVinyl = other.gameObject;
+
+                //Set vinyl pos/rot
+                Rigidbody rb = currentVinyl.GetComponent<Rigidbody>();
+                rb.useGravity = false;
+                rb.isKinematic = true;
+                currentVinyl.transform.position = vinylParent.position;
+                currentVinyl.transform.rotation = Quaternion.Euler(Vector3.zero);
+
+                anim.SetTrigger("Play");
+
+                //Set UI info
+                VinylInfo info = other.gameObject.GetComponent<VinylInfo>();
+                titleText.enabled = true;
+                descriptionText.enabled = true;
+                titleText.text = info.Title;
+                descriptionText.text = info.Description;
+                //videoPlayer.clip = info.videoPreview;
+
+                StartCoroutine(LoadSceneDelayed(info.SceneToLoad, 3));
+            }
+        }
+    }
+
+    /*
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(currentVinyl)) //If current vinyl leaves the trigger
+        {
+            //Reset physics
+            Rigidbody rb = currentVinyl.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.isKinematic = false;
+
+            currentVinyl = null;
+
+            anim.SetTrigger("Stop");
+        }
+    }
+    */
+
+    private void Update()
+    {
+        //Spin current vinyl
+        if(currentVinyl)
+        {
+            currentVinyl.transform.Rotate(0, vinylSpinSpeed, 0);
+        }
+    }
+
+    IEnumerator LoadSceneDelayed(string sceneName, int delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        cm.OnMinigameStart(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
