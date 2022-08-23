2ed6b995b333ab8fda67c33e82ae76b94c795ff8
Kent Reese <kentreese64@gmail.com>
2022-08-23 19:13:28 -0400

    Less friction

--- Assets/Scripts/ShootoutGame/ShootoutGameClientPlayer.cs
+++ Assets/Scripts/ShootoutGame/ShootoutGameClientPlayer.cs
@@ -55,7 +55,7 @@ public class ShootoutGameClientPlayer : ClientPlayer
         if (IsLocal && !isColliding) //Only read values from analog stick, and emit movement if being done from local device
         {
             // in this game we want the player to feel like they're on ice, so we calculate a low fricition 
-            float frictionCoefficient = 0.025f;
+            float frictionCoefficient = 0.015f;
             Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
 
             if (prevVector == Vector2.zero)
