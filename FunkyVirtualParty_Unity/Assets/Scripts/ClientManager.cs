374fb8ccbb72f20489c3223bd02c14db59b3768c
Thomas Ratliff <42788482+Tomasay@users.noreply.github.com>
2022-09-13 21:54:50 -0400

    Removed /new from url

--- FunkyVirtualParty_Unity/Assets/Scripts/ClientManager.cs
+++ FunkyVirtualParty_Unity/Assets/Scripts/ClientManager.cs
@@ -34,7 +34,7 @@ public class ClientManager : MonoBehaviour
 
     SocketManager manager;
     private string unityClientID = "";
-    private const string url = "https://vrpartygame.herokuapp.com/new";
+    private const string url = "https://vrpartygame.herokuapp.com/";
     private const string socketUrl = url + "socket.io/";
 
     private const int PASSCODE_LENGTH = 4;
