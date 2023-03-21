f8826220536e1f06ba8b8628590ca73c0de2e8d1
Kent Reese <kentreese64@gmail.com>
2023-03-20 21:24:37 -0400

    Kaiju Collision Detection

--- FunkyVirtualParty_Unity/Assets/Scripts/Kaiju/KaijuBehavior.cs
+++ FunkyVirtualParty_Unity/Assets/Scripts/Kaiju/KaijuBehavior.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class KaijuBehavior : MonoBehaviour
 {
     [SerializeField]
-    public int stature = 100;
+    public int stature = 30;
     public int getup = 100;
     public int offense = 100;
     public int health = 100;
@@ -14,6 +14,8 @@ public class KaijuBehavior : MonoBehaviour
     [SerializeField]
     Transform Target;
 
+    //for regenerating stature
+    float counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,15 +25,34 @@ public class KaijuBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        counter += Time.deltaTime;
         Vector3 diff = Target.position - gameObject.transform.position;
         Debug.Log(diff);
-        gameObject.transform.position += (diff.normalized * speed * Time.deltaTime);
+        gameObject.transform.position += System.Convert.ToInt32(stature > 0) * (diff.normalized * speed * Time.deltaTime);
+        if (counter > 1)
+        {
+            stature++;
+            counter = 0;
+        }
+
     }
 
     void TakeDamage(int dmg) 
     {
         // this variable controls whether the kaiju is moving
         stature = System.Convert.ToInt32(stature > 0) * (stature - (dmg / 2)); // Why do I have to do this conversion????
+        health -= System.Convert.ToInt32(stature < 0) * dmg;
+
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if( collision.gameObject.transform.root.tag.Equals("Player") )
+        {
+            TakeDamage(10);
+        }
+    }
+
+
 }
