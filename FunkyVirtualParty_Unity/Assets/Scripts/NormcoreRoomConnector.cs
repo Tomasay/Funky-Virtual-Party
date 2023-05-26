35e3ff2b34f675f3fd508f19acc8ef49ed72add6
Tomasay <42788482+Tomasay@users.noreply.github.com>
2023-05-26 00:45:40 -0400

    Added functionality for invalid party codes

--- FunkyVirtualParty_Unity/Assets/Scripts/NormcoreRoomConnector.cs
+++ FunkyVirtualParty_Unity/Assets/Scripts/NormcoreRoomConnector.cs
@@ -9,38 +9,101 @@ public class NormcoreRoomConnector : MonoBehaviour
 {
     ClientPlayer localPlayer;
 
+    RealtimeAvatarManager avatarManager;
+
     [SerializeField]
     Realtime realtime;
 
     [SerializeField]
-    TMP_InputField nameInput, roomCodeInput;
+    GameObject[] objectsToEnableOnJoin;
+
+    [SerializeField]
+    TMP_InputField nameInput, partyCodeInput;
+
+    [SerializeField]
+    GameObject partyCodeInvalid;
+
+    [SerializeField]
+    Button submitButton;
 
     [SerializeField] Button enableCustomizationsButton;
 
     [SerializeField]
-    Canvas joinRoomCanvas, controllerCanvas;
+    Canvas joinRoomCanvas, controllerCanvas, logoCanvas;
+
+    [SerializeField]
+    GameObject loadingCircle;
+
+    bool disconnectingDueToNoHost;
 
     public ClientPlayer LocalPlayer { get => localPlayer; }
 
     private void Awake()
     {
+        avatarManager = GetComponent<RealtimeAvatarManager>();
+
         realtime.didConnectToRoom += ConnectedToRoom;
+        realtime.didDisconnectFromRoom += CheckDisconnectedDueToNoHost;
     }
 
     public void SubmitButtonPressed()
     {
-        realtime.Connect(roomCodeInput.text);
+        realtime.Connect(partyCodeInput.text);
+
+        //Display loading indicator
+        joinRoomCanvas.enabled = false;
+        loadingCircle.SetActive(true);
     }
 
     void ConnectedToRoom(Realtime realtime)
     {
-        joinRoomCanvas.enabled = false;
-        controllerCanvas.enabled = true;
-        enableCustomizationsButton.gameObject.SetActive(true);
+        Debug.Log("Avatars in room: " + avatarManager.avatars.Count);
 
-        GameObject newPlayer = Realtime.Instantiate("ClientPlayer", Realtime.InstantiateOptions.defaults);
-        localPlayer = newPlayer.GetComponent<ClientPlayer>();
-        localPlayer.syncer.Name = nameInput.text;
-        localPlayer.IsLocal = true;
+        if (avatarManager.avatars.Count > 0)
+        {
+            loadingCircle.SetActive(false);
+
+            foreach (GameObject g in objectsToEnableOnJoin)
+            {
+                g.SetActive(true);
+            }
+
+            logoCanvas.enabled = false;
+            controllerCanvas.enabled = true;
+            enableCustomizationsButton.gameObject.SetActive(true);
+
+            GameObject newPlayer = Realtime.Instantiate("ClientPlayer", Realtime.InstantiateOptions.defaults);
+            localPlayer = newPlayer.GetComponent<ClientPlayer>();
+            localPlayer.syncer.Name = nameInput.text;
+            localPlayer.IsLocal = true;
+        }
+        else
+        {
+            realtime.Disconnect();
+            disconnectingDueToNoHost = true;
+        }
+    }
+
+    void CheckDisconnectedDueToNoHost(Realtime realtime)
+    {
+        if(disconnectingDueToNoHost)
+        {
+            loadingCircle.SetActive(false);
+
+            joinRoomCanvas.enabled = true;
+            partyCodeInvalid.gameObject.SetActive(true);
+
+            disconnectingDueToNoHost = false;
+        }
+    }
+
+    public void CheckValidPartyCode(string val)
+    {
+        submitButton.interactable = (val.Length == 4);
+    }
+
+    public void PartyCodeInputToUpper(string val)
+    {
+        partyCodeInput.text = val.ToUpper();
     }
 }
