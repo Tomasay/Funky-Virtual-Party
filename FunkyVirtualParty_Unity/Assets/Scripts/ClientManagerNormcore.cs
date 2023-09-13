d09ca62f09ffaa014a218d735cbaaed85a70af45
Tomasay <42788482+Tomasay@users.noreply.github.com>
2023-09-12 23:22:10 -0400

    added text for connection status, fixed flickering unity logo at start

--- FunkyVirtualParty_Unity/Assets/Scripts/ClientManagerNormcore.cs
+++ FunkyVirtualParty_Unity/Assets/Scripts/ClientManagerNormcore.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Normal.Realtime;
 using TMPro;
 
 public class ClientManagerNormcore : MonoBehaviour
 {
     [SerializeField]
-    TMP_Text partyCodeText;
+    TMP_Text linkInfoText, partyCodeText, connectingText, serverConnectionErrorText, internetConnectionErrorText;
+
+    [SerializeField]
+    Image loadingIcon, errorIcon;
+
+    [SerializeField]
+    Camera connectingUICamera;
 
     private const int PASSCODE_LENGTH = 4;
 
     void Start()
     {
+        RealtimeSingleton.instance.Realtime.didConnectToRoom += Realtime_didConnectToRoom;
+        RealtimeSingleton.instance.Realtime.didDisconnectFromRoom += Realtime_didDisconnectFromRoom;
+
         if (!RealtimeSingleton.instance.Realtime.connected)
         {
             RealtimeSingleton.instance.Realtime.Connect(GenerateCode());
@@ -20,7 +30,72 @@ public class ClientManagerNormcore : MonoBehaviour
         else
         {
             partyCodeText.text = "Party Code: " + RealtimeSingleton.instance.Realtime.room.name;
+
+            loadingIcon.enabled = false;
+            connectingText.enabled = false;
+            serverConnectionErrorText.enabled = false;
+            internetConnectionErrorText.enabled = false;
+            errorIcon.enabled = false;
+
+            linkInfoText.enabled = true;
+            partyCodeText.enabled = true;
+
+            connectingUICamera.enabled = false;
         }
+    }
+
+    private void Update()
+    {
+        if (RealtimeSingleton.instance.Realtime.connecting)
+        {
+            loadingIcon.enabled = true;
+            connectingText.enabled = true;
+            serverConnectionErrorText.enabled = false;
+            internetConnectionErrorText.enabled = false;
+            errorIcon.enabled = false;
+
+            linkInfoText.enabled = false;
+            partyCodeText.enabled = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RealtimeSingleton.instance.Realtime.didConnectToRoom -= Realtime_didConnectToRoom;
+        RealtimeSingleton.instance.Realtime.didDisconnectFromRoom -= Realtime_didDisconnectFromRoom;
+    }
+
+    private void Realtime_didDisconnectFromRoom(Realtime realtime)
+    {
+        loadingIcon.enabled = false;
+        connectingText.enabled = false;
+        errorIcon.enabled = true;
+
+        linkInfoText.enabled = false;
+        partyCodeText.enabled = false;
+
+        //Check to see if error is because device is not connected to internet
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            internetConnectionErrorText.enabled = true;
+        }
+        else
+        {
+            serverConnectionErrorText.enabled = true;
+        }
+    }
+
+    private void Realtime_didConnectToRoom(Realtime realtime)
+    {
+        loadingIcon.enabled = false;
+        connectingText.enabled = false;
+        serverConnectionErrorText.enabled = false;
+        internetConnectionErrorText.enabled = false;
+
+        linkInfoText.enabled = true;
+        partyCodeText.enabled = true;
+
+        connectingUICamera.enabled = false;
     }
 
     private string GenerateCode()
