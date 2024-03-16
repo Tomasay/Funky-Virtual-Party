2d88d23a8df2162547169d1f9c6703b12d397eb7
Kent Reese <kentreese64@gmail.com>
2024-03-16 18:39:19 -0400

    Adding BuildCommand

---
+++ FunkyVirtualParty_Unity/Assets/Scripts/Editor/BuildCommand.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildCommand : MonoBehaviour
+{
+    static void BuildWebGL()
+    {
+        // Build the player.\
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { 
+        "Assets/Scenes/MainMenu/MainMenuClient.unity",
+        "Assets/Scenes/ChaseGame/ChaseGameClient.unity",
+        "Assets/Scenes/Shootout/ShootoutClient.unity",
+        "Assets/Scenes/Kaiju/KaijuClient.unity",
+        "Assets/Scenes/VRTistry/VRTistryClient.unity",
+        "Assets/Scenes/MazeGame/MazeGameClient.unity" };
+        buildPlayerOptions.locationPathName = "yourLocationNameHere";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None; // set whatever you want here
+        buildPlayerOptions.targetGroup = BuildTargetGroup.WebGL;
+        PlayerSettings.colorSpace = ColorSpace.Gamma;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);  // apply the setting changes
+
+
+    }
+}
