9e616597e1ca0e41180cf6aae38b019e2f22bcc7
Tomasay <42788482+Tomasay@users.noreply.github.com>
2025-08-14 21:09:07 -0700

    Improved client answer score animations

--- FunkyVirtualParty_Unity/Assets/Scripts/3DPaint/AnswerOptionButton.cs
+++ FunkyVirtualParty_Unity/Assets/Scripts/3DPaint/AnswerOptionButton.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
-using NaughtyAttributes;
 
 public class AnswerOptionButton : MonoBehaviour
 {
@@ -143,19 +142,6 @@ public class AnswerOptionButton : MonoBehaviour
         }
     }
 
-    public void TestPositioning()
-    {
-        foreach (Image i in playerIcons)
-        {
-            if (i.gameObject.activeSelf)
-            {
-                Vector3 localPos = (i.transform as RectTransform).localPosition;
-                i.GetComponent<LayoutElement>().ignoreLayout = true;
-                (i.transform as RectTransform).localPosition = localPos;
-            }
-        }
-    }
-
     public void AnimateScores()
     {
         StartCoroutine("AnimateScoresCoroutine");
@@ -163,38 +149,22 @@ public class AnswerOptionButton : MonoBehaviour
 
     IEnumerator AnimateScoresCoroutine()
     {
-        //Scale player icon down
-        foreach (Image i in playerIcons)
-        {
-            if (i.gameObject.activeSelf)
-            {
-                (playerIcons[0].transform as RectTransform).DOScale(0, 0.25f);
-                yield return new WaitForSeconds(0.25f);
-            }
-        }
-
-        //Get intial positions for names
-        List<Vector3> initialPositions = new List<Vector3>();
-        foreach (Image i in playerIcons)
-        {
-            initialPositions.Add((i.transform as RectTransform).localPosition);
-        }
-
-        //Convert to score and scale back up
         for (int i = 0; i < playerIcons.Length; i++)
         {
             if (playerIcons[i].gameObject.activeSelf)
             {
+                //Scale player icon down
+                (playerIcons[i].transform as RectTransform).DOScale(0, 0.25f);
+                yield return new WaitForSeconds(0.25f);
+
+                //Convert to score and scale back up
                 SetImageAlpha(playerIcons[i].GetComponentsInChildren<Image>()[0], 0);
                 SetImageAlpha(playerIcons[i].GetComponentsInChildren<Image>()[1], 0);
                 playerIcons[i].GetComponentInChildren<TMP_Text>().text = "" + ThreeDPaintGlobalVariables.POINTS_CLIENT_CORRECT_GUESS;
                 playerIcons[i].GetComponentInChildren<TMP_Text>().color = Color.green;
                 playerIcons[i].GetComponentInChildren<TMP_Text>().fontStyle = FontStyles.Bold;
 
-                playerIcons[i].GetComponent<LayoutElement>().ignoreLayout = true;
-                (playerIcons[i].transform as RectTransform).localPosition = initialPositions[i];
-
-                (playerIcons[0].transform as RectTransform).DOScale(1, 0.25f);
+                (playerIcons[i].transform as RectTransform).DOScale(1.5f, 0.25f);
 
                 yield return new WaitForSeconds(0.25f);
             }
@@ -202,40 +172,35 @@ public class AnswerOptionButton : MonoBehaviour
 
         yield return new WaitForSeconds(1);
 
+        //Ignore layout so they can be translated. Has to be done all at once before any translations to avoid layout rearrangement
+        foreach (Image i in playerIcons)
+        {
+            if (i.gameObject.activeSelf)
+            {
+                i.GetComponent<LayoutElement>().ignoreLayout = true;
+            }
+        }
+
         //Translate to corresponding player
         foreach (Image i in playerIcons)
         {
             if (i.gameObject.activeSelf)
             {
                 ClientPlayer cp = ClientPlayer.GetClientByCurrentOwnerID(int.Parse(i.gameObject.name));
-                (playerIcons[0].transform as RectTransform).DOMove(cp.transform.position, 1);
+                (i.transform as RectTransform).DOMove(cp.transform.position, 1);
                 i.GetComponentInChildren<TMP_Text>().DOColor(Color.clear, 1);
+
+                float nameHeight = cp.playerNameText.transform.localPosition.y;
+                cp.playerNameText.DOColor(Color.green, 0.25f);
+                cp.playerNameText.transform.DOLocalMoveY(nameHeight + 5, 0.25f);
+
+                yield return new WaitForSeconds(0.25f);
+
+                cp.playerNameText.DOColor(Color.black, 0.25f);
+                cp.playerNameText.transform.DOLocalMoveY(nameHeight, 0.25f);
             }
         }
     }
-
-    /*
-     IEnumerator AnimateScoresCoroutine()
-    {
-        Image i = playerIcons[0];
-        (playerIcons[0].transform as RectTransform).DOScale(0, 0.25f);
-
-        yield return new WaitForSeconds(0.25f);
-
-        SetImageAlpha(i.GetComponentsInChildren<Image>()[0], 0);
-        SetImageAlpha(i.GetComponentsInChildren<Image>()[1], 0);
-        i.GetComponentInChildren<TMP_Text>().text = "500";
-        i.GetComponentInChildren<TMP_Text>().color = Color.green;
-        i.GetComponent<LayoutElement>().ignoreLayout = true;
-
-        (playerIcons[0].transform as RectTransform).DOScale(1, 0.25f);
-
-        yield return new WaitForSeconds(2);
-
-        (playerIcons[0].transform as RectTransform).DOMove(testPos.position, 1);
-        i.GetComponentInChildren<TMP_Text>().DOColor(Color.clear, 1);
-    }
-     * */
 
     void SetImageAlpha(Image i, float a)
     {
